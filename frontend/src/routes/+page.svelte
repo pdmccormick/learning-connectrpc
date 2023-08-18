31f201fc4b1c4d6cf4338f350abc262e35677b41
Peter David McCormick <peter@pdmccormick.com>
2023-08-18 18:04:09 -0400

    frontend: add chat functionality

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,26 +1,56 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { createChatClient } from '$lib/rpc';
+	import { writable } from 'svelte/store';
+	import ChatInput from './ChatInput.svelte';
 
 	const client = createChatClient();
 
-	let message = '';
+	let channel = writable([] as string[]);
+	let error = '';
 
-	onMount(async () => {
+	async function sendMessage(message: string) {
 		try {
-			let response = await client.say({
-				message: 'Hello World!'
+			let resp = await client.say({
+				message
 			});
 
-			message = JSON.stringify(response);
+			console.log('say response', resp);
 		} catch (e) {
-			message = `ERROR: ${e}`;
+			error = `ERROR: ${e}`;
+		}
+	}
+
+	onMount(async () => {
+		for await (const resp of client.listen({})) {
+			console.log('listen', resp);
+
+			channel.update((messages) => {
+				messages.push(resp.message);
+				return messages;
+			});
 		}
 	});
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Chat</h1>
 
-<h1>Response</h1>
-<pre>{message} </pre>
+{#if error}
+	<p>ERROR: {error}</p>
+{/if}
+
+<ul class="channel">
+	{#each $channel as message, i (i)}
+		{@const k = i + 1}
+		<li><b>#{k}.</b> {message}</li>
+	{/each}
+</ul>
+
+<ChatInput on:send={(ev) => sendMessage(ev.detail.message)} />
+
+<style>
+	.channel {
+		display: flex;
+		flex-direction: column-reverse;
+	}
+</style>
---
+++ frontend/src/routes/ChatInput.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+
+	let dispatch = createEventDispatcher<{
+		send: {
+			message: string;
+		};
+	}>();
+
+	let message: string;
+
+	function onSend() {
+		dispatch('send', { message });
+		message = '';
+	}
+</script>
+
+<div>
+	<form on:submit|preventDefault={onSend}>
+		<!-- svelte-ignore a11y-autofocus -->
+		<input type="text" bind:value={message} autofocus={true} />
+		<button type="submit">Send!</button>
+	</form>
+</div>
